a1b33bf8b4fde6be3503dc7e8e4813f6059b7c9f
popcorny <celu@infuseai.io>
2021-12-29 11:15:11 +0800

    Support multiclass
    Signed-off-by: popcorny <celu@infuseai.io>

--- train.py
+++ train.py
@@ -1,76 +1,64 @@
-# import matplotlib.pyplot as plt
 import numpy as np
 import os
 import tensorflow as tf
 
-def train(train_dir, validation_dir=None, batch_size=32, initial_epachs=1, finetune_epochs=1, image_size=(160,160)):
+
+def train(train_dir, validation_dir=None, batch_size=32, epochs=1, image_size=(160, 160)):
+    # Step1: Data prep
     train_dataset = tf.keras.utils.image_dataset_from_directory(train_dir,
-                                                            shuffle=True,
-                                                            label_mode="categorical",
-                                                            batch_size=batch_size,
-                                                            image_size=image_size)
+                                                                shuffle=True,
+                                                                batch_size=batch_size,
+                                                                image_size=image_size)
     class_names = train_dataset.class_names
 
     if validation_dir:
-      validation_dataset = tf.keras.utils.image_dataset_from_directory(validation_dir,
-                                                                  shuffle=True,
-                                                                  label_mode="categorical",
-                                                                  batch_size=batch_size,
-                                                                  image_size=image_size)
-      batches = tf.data.experimental.cardinality(validation_dataset)
-      if batches == 1:
-        test_dataset = validation_dataset
-      elif batches < 5:
-        test_dataset = validation_dataset.take(batches // 2)
-        validation_dataset = validation_dataset.skip(batches // 2)
-      else:
-        test_dataset = validation_dataset.take(batches // 5)
-        validation_dataset = validation_dataset.skip(batches // 5)
+        validation_dataset = tf.keras.utils.image_dataset_from_directory(validation_dir,
+                                                                         shuffle=True,
+                                                                         batch_size=batch_size,
+                                                                         image_size=image_size)
+        batches = tf.data.experimental.cardinality(validation_dataset)
+        if batches == 1:
+            test_dataset = validation_dataset
+        elif batches < 5:
+            test_dataset = validation_dataset.take(batches // 2)
+            validation_dataset = validation_dataset.skip(batches // 2)
+        else:
+            test_dataset = validation_dataset.take(batches // 5)
+            validation_dataset = validation_dataset.skip(batches // 5)
     else:
-      test_dataset = None
-      validation_dataset = None
+        test_dataset = None
+        validation_dataset = None
 
-    print('Number of trian batches: %d' % tf.data.experimental.cardinality(train_dataset))
+    print('Number of trian batches: %d' %
+          tf.data.experimental.cardinality(train_dataset))
     if validation_dataset:
-      print('Number of validation batches: %d' % tf.data.experimental.cardinality(validation_dataset))
-      print('Number of test batches: %d' % tf.data.experimental.cardinality(test_dataset))
-
-
+        print('Number of validation batches: %d' %
+              tf.data.experimental.cardinality(validation_dataset))
+        print('Number of test batches: %d' %
+              tf.data.experimental.cardinality(test_dataset))
     AUTOTUNE = tf.data.AUTOTUNE
     train_dataset = train_dataset.prefetch(buffer_size=AUTOTUNE)
     if validation_dataset:
-      validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
-      test_dataset = test_dataset.prefetch(buffer_size=AUTOTUNE)
+        validation_dataset = validation_dataset.prefetch(buffer_size=AUTOTUNE)
+        test_dataset = test_dataset.prefetch(buffer_size=AUTOTUNE)
 
+    # Step2: Model Architecture
     data_augmentation = tf.keras.Sequential([
-      tf.keras.layers.RandomFlip('horizontal'),
-      tf.keras.layers.RandomRotation(0.2),
+        tf.keras.layers.RandomFlip('horizontal'),
+        tf.keras.layers.RandomRotation(0.2),
     ])
-
     preprocess_input = tf.keras.applications.mobilenet_v2.preprocess_input
-    # rescale = tf.keras.layers.Rescaling(1./127.5, offset=-1)
-
-    # Create the base model from the pre-trained model MobileNet V2
     image_shape = image_size + (3,)
     base_model = tf.keras.applications.MobileNetV2(input_shape=image_shape,
-                                               include_top=False,
-                                               weights='imagenet')
+                                                   include_top=False,
+                                                   weights='imagenet')
+    base_model.trainable = True
+    fine_tune_at = 100
+    for layer in base_model.layers[:fine_tune_at]:
+        layer.trainable = False
 
-    image_batch, label_batch = next(iter(train_dataset))
-    feature_batch = base_model(image_batch)
-    # print(feature_batch.shape)
-
-
-    # Feature Extaction
-    base_model.trainable = False
-    # Let's take a look at the base model architecture
-    # base_model.summary()
     global_average_layer = tf.keras.layers.GlobalAveragePooling2D()
-    feature_batch_average = global_average_layer(feature_batch)
-    # print(feature_batch_average.shape)
     prediction_layer = tf.keras.layers.Dense(len(class_names))
-    prediction_batch = prediction_layer(feature_batch_average)
-    # print(prediction_batch.shape)
     inputs = tf.keras.Input(shape=image_shape)
     x = data_augmentation(inputs)
     x = preprocess_input(x)
@@ -81,79 +69,45 @@ def train(train_dir, validation_dir=None, batch_size=32, initial_epachs=1, finet
     model = tf.keras.Model(inputs, outputs)
     base_learning_rate = 0.0001
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=base_learning_rate),
-              loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
-              metrics=['accuracy'])
+                  loss=tf.keras.losses.SparseCategoricalCrossentropy(
+                      from_logits=True),
+                  metrics=['accuracy'])
     model.summary()
-    # len(model.trainable_variables)
 
-    # if validation_dataset:
-    #   loss0, accuracy0 = model.evaluate(validation_dataset)
-    #   print("initial loss: {:.2f}".format(loss0))
-    #   print("initial accuracy: {:.2f}".format(accuracy0))
-
+    # Step 3: Start training
     history = model.fit(train_dataset,
-                    epochs=initial_epachs,
-                    validation_data=validation_dataset)
+                        epochs=epochs,
+                        validation_data=validation_dataset)
 
-    # acc = history.history['accuracy']
-    # loss = history.history['loss']
-    # if validation_dataset:
-    #   val_acc = history.history['val_accuracy']
-    #   val_loss = history.history['val_loss']
-
-    # Finetune
-    base_model.trainable = True
-    # print("Number of layers in the base model: ", len(base_model.layers))
-    fine_tune_at = 100
-    for layer in base_model.layers[:fine_tune_at]:
-      layer.trainable =  False
-    model.compile(loss=tf.keras.losses.CategoricalCrossentropy(from_logits=True),
-              optimizer = tf.keras.optimizers.RMSprop(learning_rate=base_learning_rate/10),
-              metrics=['accuracy'])
-    model.summary()
-    # len(model.trainable_variables)
-
-    total_epochs =  initial_epachs + finetune_epochs
-
-    history_fine = model.fit(train_dataset,
-                         epochs=total_epochs,
-                         initial_epoch=history.epoch[-1],
-                         validation_data=validation_dataset)
-    # acc += history_fine.history['accuracy']
-    # loss += history_fine.history['loss']
-    # if validation_dataset:
-    #   val_acc += history_fine.history['val_accuracy']
-    #   val_loss += history_fine.history['val_loss']
-
+    # Step 4: Evaluation
     if test_dataset:
-      loss, accuracy = model.evaluate(test_dataset)
-      print('Test accuracy :', accuracy)
+        loss, accuracy = model.evaluate(test_dataset)
+        print('Test accuracy :', accuracy)
 
-      # Retrieve a batch of images from the test set
-      image_batch, label_batch = test_dataset.as_numpy_iterator().next()
-      predictions = model.predict_on_batch(image_batch).flatten()
+        # Retrieve a batch of images from the test set
+        image_batch, label_batch = test_dataset.as_numpy_iterator().next()
+        predictions = model.predict_on_batch(image_batch)
 
-      # Apply a sigmoid since our model returns logits
-      predictions = tf.nn.sigmoid(predictions)
-      predictions = tf.where(predictions < 0.5, 0, 1)
+        # Apply softmax and argmax to find the most possible class
+        predictions = model.predict_on_batch(image_batch)
+        predictions = tf.nn.softmax(predictions)
+        predictions = tf.math.argmax(predictions, axis=-1)
+        print('Predictions:\n', predictions.numpy())
+        print('Labels:\n', label_batch)
 
-      print('Predictions:\n', predictions.numpy())
-      print('Labels:\n', label_batch)
+    return (model, class_names, history)
+
 
 # Main
 # _URL = 'https://storage.googleapis.com/mledu-datasets/cats_and_dogs_filtered.zip'
-# path_to_zip = tf.keras.utils.get_file('cats_and_dogs.zip', origin=_URL, extract=True)
+# path_to_zip = tf.keras.utils.get_file(
+#     'cats_and_dogs.zip', origin=_URL, extract=True)
 # PATH = os.path.join(os.path.dirname(path_to_zip), 'cats_and_dogs_filtered')
-# print(PATH)
-# train_dir = os.path.join(PATH, 'train')
-# validation_dir = os.path.join(PATH, 'validation')
-
+PATH='datasets/cats_and_dogs_50'
+train_dir = os.path.join(PATH, 'train')
+validation_dir = os.path.join(PATH, 'validation')
 
 BATCH_SIZE = 32
-INITIAL_EPOCHS = 10
-FINETUNE_EPOCHS = 10
-IMG_SIZE = (100, 100)
-# train(train_dir, validation_dir, BATCH_SIZE, INITIAL_EPOCHS, FINETUNE_EPOCHS, IMG_SIZE)
-# train(validation_dir)
-train('cats_and_dogs_50/train', 'cats_and_dogs_50/validation', initial_epachs=10, finetune_epochs=10)
-
+EPOCHS = 2
+IMG_SIZE = (160, 160)
+train(train_dir, validation_dir, BATCH_SIZE, EPOCHS, IMG_SIZE)
